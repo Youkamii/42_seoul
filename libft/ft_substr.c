a5924c5a7bf68ccba1a4217e73c42d31dcf1b359
Youkamii <gkfkd747@naver.com>
2022-04-12 13:24:48 +0900

    test

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: lia <lia@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 17:40:45 by chyeok            #+#    #+#             */
-/*   Updated: 2022/03/24 17:40:47 by chyeok           ###   ########.fr       */
+/*   Updated: 2022/04/11 23:11:55 by lia              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,23 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*str;
+	size_t			i;
+	size_t			j;
 
+	if (!s)
+		return (0);
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
-	
+	i = 0;
+	j = ft_strlen(s);
+	if (j < start)
+		return (ft); //strdup?
+	while (i <= len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
