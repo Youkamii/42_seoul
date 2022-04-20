32349ffcfe0c9519e5c81a8fe7a04aa9e7922767
Youkamii <gkfkd747@naver.com>
2022-04-20 18:08:33 +0900

    memcpy 예외처리 추가

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -17,9 +17,14 @@ void	*ft_memcpy(void *dest, const void *src, size_t size)
 	size_t	i;
 
 	i = 0;
+	if (dest == 0 && src == 0)
+			return (NULL);
+	if (size == 0)
+		return (dest);
 	while (i < size)
 	{
 		*(unsigned char *)(dest + i) = *(unsigned char *)(src + i);
+		i++;
 	}
 	return (dest);
 }
