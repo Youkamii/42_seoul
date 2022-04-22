bbc87ef7cef896536f4c16a108f32d1b85db9286
Youkamii <gkfkd747@naver.com>
2022-04-22 20:58:51 +0900

    substr 수정2

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -18,20 +18,20 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t			i;
 	size_t			j;
 
-	if (!s)
-		return (0);
-	str = (char *)malloc(sizeof(char) * (len + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	j = ft_strlen(s);
-	if (j < start)
-		return (ft); // 수정해야함
-	while (i <= len)
-	{
-		str[i] = s[start + i];
-		i++;
-	}
-	str[i] = '\0';
+	// if (!s)
+	// 	return (0);
+	// str = (char *)malloc(sizeof(char) * (len + 1));
+	// if (!str)
+	// 	return (NULL);
+	// i = 0;
+	// j = ft_strlen(s);
+	// if (j < start)
+	// 	// return (ft); // 수정해야함
+	// while (i <= len)
+	// {
+	// 	str[i] = s[start + i];
+	// 	i++;
+	// }
+	// str[i] = '\0';
 	return (str);
 }
