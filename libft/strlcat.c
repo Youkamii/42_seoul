90eb8cad23f8ffc2eb77cee6fce5a49b717a8029
Youkamii <gkfkd747@naver.com>
2022-04-25 21:46:55 +0900

    strlcat

--- libft/strlcat.c
+++ libft/strlcat.c
@@ -12,4 +12,19 @@
 
 #include <string.h>
 
-size_t	strlcat(char *dest, const char *src, size_t size);
+size_t	strlcat(char *dest, const char *src, size_t size)
+{
+    size_t i;
+    size_t j;
+
+    i = 0;
+    j = 0;
+    while (dest[i] && i < size)
+        i++;
+    while (src[j] && i + j < size - 1)
+    {
+        dest[i + j] = src[j];
+        j++;
+    }
+    return (ft_strlen(dest));
+}
