5365638bfe199505ed05ea43a7d1a825f5cce1e7
Youkamii <gkfkd747@naver.com>
2022-05-02 20:13:16 +0900

    lpC3

---
+++ La_piscine/C03/ex04/ft_strstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/20 19:13:14 by chyeok            #+#    #+#             */
+/*   Updated: 2022/01/20 19:13:15 by chyeok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strstr(char *str, char *to_find);
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (*to_find == '\0')
+		return (&str[0]);
+	while (str[i] != '\0')
+	{
+		if (str[i] == to_find[0])
+		{
+			j = 0;
+			while (1)
+			{
+				if (to_find[j] == '\0')
+					return (&str[i]);
+				if (!(str[i + j] == to_find[j]))
+					break ;
+				j++;
+			}
+		}
+		i++;
+	}
+	return (0);
+}
---
+++ La_piscine/C03/ex05/ft_strlcat.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/22 10:03:45 by chyeok            #+#    #+#             */
+/*   Updated: 2022/01/22 10:03:51 by chyeok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size);
+
+unsigned int	ft_strlen(char *str);
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	unsigned int	i;
+
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	i = 0;
+	if (size > dest_len)
+	{
+		while (src[i] && dest_len + i < size - 1)
+		{
+			dest[dest_len + i] = src[i];
+			i++;
+		}
+		dest[dest_len + i] = '\0';
+	}
+	if (size < dest_len)
+		return (src_len + size);
+	else
+		return (dest_len + src_len);
+}
