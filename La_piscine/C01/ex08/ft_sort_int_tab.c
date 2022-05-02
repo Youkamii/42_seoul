49bc9679833e09bae63b332ecbd7a4b395591eac
Youkamii <gkfkd747@naver.com>
2022-05-02 20:12:51 +0900

    lpC1

---
+++ La_piscine/C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/11 16:04:19 by chyeok            #+#    #+#             */
+/*   Updated: 2022/01/11 16:04:25 by chyeok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	j = -1;
+	while (j < size - 1)
+	{
+		j++;
+		i = j + 1;
+		while (i < size)
+		{
+			if (tab[j] > tab[i])
+			{
+				temp = tab[j];
+				tab[j] = tab[i];
+				tab[i] = temp;
+			}
+			i++;
+		}
+	}
+}
