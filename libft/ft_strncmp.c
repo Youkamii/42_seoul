af10a6965e609d8f8b9e0a8db440281599c1e966
Youkamii <gkfkd747@naver.com>
2022-05-02 17:23:57 +0900

    str-

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,4 +1,13 @@
-char *ft_strnstr(const char *str, const char *to_find, size_t n)
+#include "libft.h"
+
+char *ft_strnstr(const char *str1, const char *str2, size_t n)
 {
-    ;
+    size_t i;
+
+    i = 0;
+    if (n == '\0')
+        return (0);
+    while (str1[i] && str2[i] && i < n - 1 && str1[i] == str2[i])
+        i++;
+    return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 }
