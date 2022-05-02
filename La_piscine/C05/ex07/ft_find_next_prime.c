e81cb9b1f66c826128279f11f2cca0f69c6bf439
Youkamii <gkfkd747@naver.com>
2022-05-02 20:13:37 +0900

    lpC5

---
+++ La_piscine/C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/24 19:26:47 by chyeok            #+#    #+#             */
+/*   Updated: 2022/01/24 19:26:57 by chyeok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 1)
+		return (0);
+	i = 2;
+	while (i <= nb / i)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (nb < 2)
+		return (2);
+	while (!(ft_is_prime(nb)))
+		nb++;
+	return (nb);
+}
