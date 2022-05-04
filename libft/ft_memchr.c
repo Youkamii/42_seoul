a891b2e21b2c0e14be42c21f02ef43bf93bac8a1
Youkamii <gkfkd747@naver.com>
2022-05-04 18:55:37 +0900

    t

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,6 +1,19 @@
 #include "libft.h"
+#include <string.h>
 
 void    *ft_memchr(const void *s, int c, size_t n)
 {
-    return ;
+    unsigned char   *tmp_s;
+    unsigned int    i;
+
+    i = n;
+    tmp_s = (unsigned char *)s;
+    while (i)
+    {
+        if (*tmp_s == c)
+            return (tmp_s);
+        tmp_s++;
+        i--;
+    }
+    return ((void *)0);
 }
---
+++ libft/ft_memcmp.c
@@ -0,0 +1,4 @@
+int *ft_memcmp(const void *dest, const void *src, size_t num)
+{
+    ;
+}
