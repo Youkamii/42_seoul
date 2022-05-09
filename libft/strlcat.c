a12ebc4ec514d4fdaf6ddc2b343f8bee17da793d
Youkamii <gkfkd747@naver.com>
2022-05-09 18:06:13 +0900

    ubt

--- libft/strlcat.c
+++ libft/strlcat.c
@@ -1,25 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   strlcat.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lia <lia@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/15 18:07:33 by chyeok            #+#    #+#             */
-/*   Updated: 2022/05/09 18:05:36 by lia              ###   ########.fr       */
+/*   Created: 2022/04/12 16:28:10 by chyeok            #+#    #+#             */
+/*   Updated: 2022/05/09 18:05:44 by lia              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include <string.h>
 
-void	*ft_calloc(size_t count, size_t size)
+size_t	strlcat(char *dest, const char *src, size_t size)
 {
-	void	*tmp;
-
-	tmp = malloc(count * size);
-	if (!tmp)
-		return (NULL);
-	ft_bzero(tmp, (count * size));
-	return (tmp);
-	
+    
 }
