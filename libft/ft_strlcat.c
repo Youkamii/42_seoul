bee27ef537665af4f1d673d50393e160e904e11b
Youkamii <gkfkd747@naver.com>
2022-05-14 15:03:05 +0900

    strlcat_refix

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -1,34 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strlcat.c                                          :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 16:28:10 by chyeok            #+#    #+#             */
-/*   Updated: 2022/04/12 16:28:11 by chyeok           ###   ########.fr       */
+/*   Updated: 2022/05/14 15:02:51 by chyeok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-size_t	strlcat(char *dest, const char *src, size_t size)
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
-    size_t i;
-    size_t j;
+	size_t	i;
+	size_t	j;
 
-    i = 0;
-    j = 0;
-    while (dest[i] && i < size)
-        i++;
-    if (i < size)
-    {
-        while (src[j] && i + j < size - 1)
-        {
-            dest[i + j] = src[j];
-            j++;
-        }
-        dest[i + j] = '\0';
-    }
-    return (ft_strlen(dest));
+	i = 0;
+	j = 0;
+	while (dest[i] && i < size)
+		i++;
+	if (i < size)
+	{
+		while (src[j] && i + j < size - 1)
+		{
+			dest[i + j] = src[j];
+			j++;
+		}
+		dest[i + j] = '\0';
+	}
+	return (ft_strlen(dest));
 }
