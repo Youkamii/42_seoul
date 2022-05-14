82bcbadde0a121646fd09e22accb6b07249d2e15
Youkamii <gkfkd747@naver.com>
2022-05-14 15:47:04 +0900

    strnstr_header_norm

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -1,17 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/14 15:46:02 by chyeok            #+#    #+#             */
+/*   Updated: 2022/05/14 15:46:03 by chyeok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char    *ft_strnstr(const char *str, const char *find, size_t len)
+char	*ft_strnstr(const char *str, const char *find, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
-	if(!*str)
+	if (!*str)
 		return ((char *)str);
-	while(str[i] && i < len)
+	while (str[i] && i < len)
 	{
 		j = 0;
-		while(str[i + j] == find[j] && i + j < len)
+		while (str[i + j] == find[j] && i + j < len)
 		{
 			if (!find[j])
 				return ((char *)&str[i]);
