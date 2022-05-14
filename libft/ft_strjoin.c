e88243f17d8f39943996b00abdb6bb2dfb25f721
Youkamii <gkfkd747@naver.com>
2022-05-14 15:01:42 +0900

    strjoin_header_미완

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/14 14:59:23 by chyeok            #+#    #+#             */
+/*   Updated: 2022/05/14 14:59:25 by chyeok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strjoin(char const *str1, char const *str2)
 {
-	char*	tmp_str;
+	char	*tmp_str;
 	size_t	len_str1;
 	size_t	len_str2;
 
@@ -10,7 +22,7 @@ char	*ft_strjoin(char const *str1, char const *str2)
 	len_str2 = ft_strlen(str2);
 	if (!str1 || !str2)
 		return (NULL);
-	tmp_str = (char *)malloc(sizeof(char * (len_str1 + len_str2 + 1)));
+	tmp_str = (char *)malloc(sizeof(char *(len_str1 + len_str2) + 1)));
 	if (!tmp_str)
 		return (NULL);
 	ft_strlcpy(tmp_str, str1, len_str1 + 1);
