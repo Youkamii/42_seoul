963ca009049bb2ccf19b648eaffe1b909ff36fdd
Youkamii <gkfkd747@naver.com>
2022-05-14 15:45:50 +0900

    strncmp_header_norm

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/14 15:43:46 by chyeok            #+#    #+#             */
+/*   Updated: 2022/05/14 15:43:47 by chyeok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-int ft_strncmp(const char *str1, const char *str2, size_t n)
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-    if (n == '\0')
-        return (0);
-    while (str1[i] && str2[i] && i < n - 1 && str1[i] == str2[i])
-        i++;
-    return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+	i = 0;
+	if (n == '\0')
+		return (0);
+	while (str1[i] && str2[i] && i < n - 1 && str1[i] == str2[i])
+		i++;
+	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 }
