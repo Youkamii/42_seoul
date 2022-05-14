f94780a509dc859ed609ef0240e817ac40e9d6a4
Youkamii <gkfkd747@naver.com>
2022-05-14 14:57:55 +0900

    strchr_header_norm

--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -1,14 +1,26 @@
-char    *ft_strchr(const char *str, int c)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/14 14:56:06 by chyeok            #+#    #+#             */
+/*   Updated: 2022/05/14 14:56:07 by chyeok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strchr(const char *str, int c)
 {
-    if (!(*str))
-        return (0);
-    else if (c == '\0')
-        return ((char *)str);
-    while (*str)
-    {
-        if(*str == c)
-            return ((char *)str);
-        ++str;
-    }
-    return (0);
+	if (!(*str))
+		return (0);
+	else if (c == '\0')
+		return ((char *)str);
+	while (*str)
+	{
+		if (*str == c)
+			return ((char *)str);
+		++str;
+	}
+	return (0);
 }
