a8161b7f11bb3021db22e4d85396d013927e863f
Youkamii <gkfkd747@naver.com>
2022-05-14 15:48:36 +0900

    strrchr_header_norm

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -1,16 +1,28 @@
-char    *ft_strchr(const char *str, int c)
-{
-    int i;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/14 15:47:32 by chyeok            #+#    #+#             */
+/*   Updated: 2022/05/14 15:47:33 by chyeok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    if (!(*str))
-        return (0);
-    else
-        i = ft_strlen(str);
-    while (i >= 0)
-    {
-        if(str[i] == c)
-            return ((char *)str[i]);
-        --i;
-    }
-    return (0);
+char	*ft_strchr(const char *str, int c)
+{
+	int	i;
+
+	if (!(*str))
+		return (0);
+	else
+		i = ft_strlen(str);
+	while (i >= 0)
+	{
+		if (str[i] == c)
+			return ((char *)str[i]);
+		--i;
+	}
+	return (0);
 }
