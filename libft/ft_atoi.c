8388d5dc67cfef6529590c47e00b03fac73058aa
Youkamii <gkfkd747@naver.com>
2022-05-14 16:02:23 +0900

    atoi_fix

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -10,12 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_atoi(const char *str)
+int	ft_atoi(const char *str)
 {
-    int i;
-    int sign;
+	int	i;
+	int	sign;
 
-    i = 0;
+	i = 0;
 	sign = 1;
 	while ((*str >= 9 && *str <= 13) || *str == 32)
 		str++;
@@ -27,11 +27,11 @@ int ft_atoi(const char *str)
 	}
 	while (*str >= '0' && *str <= '9')
 	{
-        i = i * 10 + (*str - '0');
-        if (sign == -1 && i > 2147483648)
-            return (0);
-        if (sign == 1 && i > 2147483647)
-            return (-1);
+		i = i * 10 + (*str - '0');
+		if (sign == -1 && i > 2147483648)
+			return (0);
+		if (sign == 1 && i > 2147483647)
+			return (-1);
 		str++;
 	}
 	return (i * sign);
