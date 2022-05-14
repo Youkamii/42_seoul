965603293140d1371a65bb589af3382af697a2df
Youkamii <gkfkd747@naver.com>
2022-05-14 14:47:22 +0900

    memchr_header_norm

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,19 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/14 14:45:29 by chyeok            #+#    #+#             */
+/*   Updated: 2022/05/14 14:45:30 by chyeok           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 #include <string.h>
 
-void    *ft_memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-    unsigned char   *tmp_s;
-    unsigned int    i;
+	unsigned char	*tmp_s;
+	unsigned int	i;
 
-    i = n;
-    tmp_s = (unsigned char *)s;
-    while (i)
-    {
-        if (*tmp_s == c)
-            return (tmp_s);
-        tmp_s++;
-        i--;
-    }
-    return ((void *)0);
+	i = n;
+	tmp_s = (unsigned char *)s;
+	while (i)
+	{
+		if (*tmp_s == c)
+			return (tmp_s);
+		tmp_s++;
+		i--;
+	}
+	return ((void *)0);
 }
