432f1cd89154be68d32b1310f3f8f9fa1b190c23
Youkamii <gkfkd747@naver.com>
2022-05-18 00:54:48 +0900

    striteri

--- libft/ft_striteri.c
+++ libft/ft_striteri.c
@@ -10,4 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	if (!(s || f))
+		return ;
+	len = ft_strlen(s);
+	while (i < len)
+	{
+		f(i, &s[i]);
+		++i;
+	}
+}
