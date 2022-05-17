2678477bc527497023597e8882a8e9495e58e299
Youkamii <gkfkd747@naver.com>
2022-05-18 02:57:14 +0900

    split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -10,7 +10,46 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	count_word(char const *s, char c)
+{
+	size_t	count;
+	size_t	i;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			i++;
+		else
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+	}
+	return (count);
+}
+
+char	**split_body(s, c, result, size)
+{
+	return (result);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	return (0);
+	size_t	**result;
+	size_t	size;
+
+	if (!s)
+		return (NULL);
+	size = count_word(s, c);
+	result = (char **)malloc(sizeof(char *) * (size + 1));
+	if (!result)
+		return (NULL);
+	split_body(s, c, result, size);
+	return (result);
 }
+
