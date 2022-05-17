6d3df1778075bdea1276776d2b8de4c7215042ff
Youkamii <gkfkd747@naver.com>
2022-05-18 00:15:06 +0900

    strmapi

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -10,4 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+char	*ft_strmapi(char const *str, char (*f)(unsigned int, char))
+{
+	size_t i;
+	char* res;
+
+	i = 0;
+	if (!(str || f))
+		return (0);
+	if (!(res = (char*)malloc(sizeof(char) * (ft_strlen(str) + 1))))
+		return (0);
+	while (str[i])
+	{
+		res[i] = f(i, str[i]);
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
