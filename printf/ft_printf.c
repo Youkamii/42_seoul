cb0349f3a7513481d5cd967339f352e093c36044
Youkamii <gkfkd747@naver.com>
2022-07-01 21:19:18 +0900

    pritnf.c

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,8 +6,14 @@
 /*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 21:12:14 by chyeok            #+#    #+#             */
-/*   Updated: 2022/07/01 21:12:15 by chyeok           ###   ########.fr       */
+/*   Updated: 2022/07/01 21:18:59 by chyeok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_printf(const char *str, ...)
+{
+	int	i;
+	int	len;
 
+	return (len);
+}
