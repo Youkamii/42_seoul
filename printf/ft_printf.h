290f9a751b85c6fb5781b602dc6429c439eb9d67
Youkamii <gkfkd747@naver.com>
2022-07-19 16:41:23 +0900

    printf 수정중

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -10,4 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
+# include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int		ft_printf(const char *format, ...);
+
+#endif
