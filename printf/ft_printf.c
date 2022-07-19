e25cbd34665963f15b788dba702ecedc30fd3b15
Youkamii <gkfkd747@naver.com>
2022-07-20 07:30:53 +0900

    update

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -12,25 +12,25 @@
 
 #include "ft_printf.h"
 
-int	ft_format_spc(va_list args, const char format)
+int	ft_format_spc(va_list ap, const char format)
 {
-	int	i;
-
-	i = 0;
 	if (format == 'c')
-		return ()
+		return (ft_putchar(va_arg(ap, int)));
 	else if (format == 's')
-		return
+		return (ft_putstr(va_arg(ap, char *)));
 	else if (format == 'p')
-		return
+		return (ft_putptr(va_arg(ap, void *)));
 	else if (format == 'x')
-		return
+		return (ft_putnbr(va_arg(ap, unsigned int), \
+		"0123456789abcdef", 16));
 	else if (format == 'X')
-		return
-	else if (format == 'd')
-		return
-	else (format == '%')
-		return
+		return (ft_putnbr(va_arg(ap, unsigned int), \
+		"0123456789ABCDEF", 16));
+	else if (format == 'd' || format == 'i')
+		return (ft_putnbr(va_arg(ap, int)));
+	else if (format == '%')
+		return (ft_putchar('%'));
+	return (0);
 }
 
 int	ft_printf(const char *format, ...)
@@ -40,6 +40,7 @@ int	ft_printf(const char *format, ...)
 	int		res;
 
 	res = 0;
+	va_start(ap, format);
 	if (format == NULL)
 		return (-1);
 	else
@@ -48,12 +49,14 @@ int	ft_printf(const char *format, ...)
 		{
 			if (format[i] == '%')
 			{
-				res += 
+				res += ft_format_spc(ap, format[i + 1]);
+				cur++;
 			}
+			else
+				res += ft_putchar(format[i]);
 			cur++;
 		}
 	}
-	va_start(ap, format);
 	va_end(ap);
 	return (res);
 }
