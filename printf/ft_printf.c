78a72043a93959f044e8a9b08c7f0b043a822bca
Lia Choi <liachoi@Liaui-MacBookAir.local>
2022-07-19 12:45:23 +0900

    mac

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -3,12 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
+/*   By: liachoi <liachoi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 21:12:14 by chyeok            #+#    #+#             */
-/*   Updated: 2022/07/01 21:18:59 by chyeok           ###   ########.fr       */
+/*   Updated: 2022/07/19 12:44:56 by liachoi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "ft_printf.h"
 
 int	ft_printf(const char *str, ...)
 {
@@ -16,4 +18,31 @@ int	ft_printf(const char *str, ...)
 	int	len;
 
 	return (len);
+}
+
+int	ft_format(va_list args, const char format)
+{
+	int	i;
+
+	i = 0;
+	if (format == 'c')
+		return ()
+	else if (format == 's')
+		return
+	else if (format == 'p')
+		return
+	else if (format == 'x')
+		return
+	else if (format == 'X')
+		return
+	else if (format == 'd')
+		return
+	else (format == '%')
+		return
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	
 }
