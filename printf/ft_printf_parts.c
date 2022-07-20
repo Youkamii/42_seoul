43953fb5ccb073a53e6db44d1af74e858ae03233
Youkamii <gkfkd747@naver.com>
2022-07-20 15:42:23 +0900

    왜 되는거야...?

--- printf/ft_printf_parts.c
+++ printf/ft_printf_parts.c
@@ -6,26 +6,22 @@
 /*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 07:39:26 by chyeok            #+#    #+#             */
-/*   Updated: 2022/07/20 10:08:25 by chyeok           ###   ########.fr       */
+/*   Updated: 2022/07/20 15:41:24 by chyeok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_putstr(char *s)
+int	ft_putstr(char *s)
 {
-	int	i;
-
 	if (!s)
 	{
 		write(1, "(null)", 6);
 		return (6);
 	}
-	i = ft_strlen(s);
-	write(1, s, 1);
-	return (i);
+	write(1, s, ft_strlen(s));
+	return (ft_strlen(s));
 }
-
 
 int	ft_putnbr(int n)
 {
@@ -57,11 +53,11 @@ int	ft_putptr(void *ptr)
 
 	i = 0;
 	i += ft_putstr("0x");
-	i += ft_putnbr_p((unsigned long int)ptr);
+	i += ft_putnbr_ptr((unsigned long int)ptr);
 	return (i);
 }
 
-int	ft_putnbr_p(unsigned long int n)
+int	ft_putnbr_ptr(unsigned long int n)
 {
 	int	nbr;
 	int	i;
