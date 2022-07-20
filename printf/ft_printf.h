137ed5f020ade55175344bcc7d0b7881e8186ee9
Youkamii <gkfkd747@naver.com>
2022-07-20 10:29:52 +0900

    update

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -20,6 +20,11 @@
 int		ft_printf(const char *format, ...);
 int		ft_format_type(va_list ap, const char format);
 int		ft_putchar(char c);
-int     ft_putstr(char *s);
+int		ft_strlen(const char *str);
+int		ft_putstr(char *s);
+int		ft_putptr(void *ptr);
+int		ft_putnbr(int n);
+int		ft_putnbr_ptr(unsigned long int n);
+int		ft_putnbr_u(unsigned int n, char *base, int base_n);
 
 #endif
--- printf/ft_printf_parts.c
+++ printf/ft_printf_parts.c
@@ -26,14 +26,64 @@ int ft_putstr(char *s)
 	return (i);
 }
 
-int ft_putnbr
-
 int	ft_putptr(void *ptr)
 {
 	int	i;
 
 	i = 0;
 	i += ft_putstr("0x");
-	i += ft_putnbr((unsigned long int)ptr);
+	i += ft_putnbr_ptr((unsigned long int)ptr);
+	return (i);
+}
+
+int	ft_putnbr(int n)
+{
+	int	nbr;
+	int	i;
+
+	i = 0;
+	if (n == -2147483648)
+		return (ft_putstr_fd("-2147483648"));
+	else if (n < 0)
+	{
+		write(1, "-", 1);
+		i += ft_putnbr_fd(-n) + 1;
+	}
+	else
+	{
+		nbr = n % 10;
+		n /= 10;
+		if (n >= 1)
+			i += ft_putnbr(n);
+		i += ft_putchar("0123456789"[nbr]);
+	}
+	return (i);
+}
+
+int	ft_putnbr_ptr(unsigned long int n)
+{
+	int	nbr;
+	int	i;
+
+	i = 0;
+	nbr = n % 16;
+	n /= 16;
+	if (n >= 1)
+		i += ft_putnbr_ptr(n);
+	i += ft_putchar("0123456789abcdef"[nbr]);
+	return (i);
+}
+
+int	ft_putnbr_u(unsigned int n, char *base, int base_n)
+{
+	int	nbr;
+	int	i;
+
+	i = 0;
+	nbr = n % base_n;
+	n /= base_n;
+	if (n >= 1)
+		i += ft_putnbr_u(n, base, base_n);
+	i += ft_putchar(base[nbr]);
 	return (i);
 }
