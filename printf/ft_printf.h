eae3b1d26b2efb1b7e27e9d5789df10ec1a8a2cc
Lia Choi <liachoi@192.168.nate.com>
2022-07-29 15:19:00 +0900

    del_errorfiles_2

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,11 +6,8 @@
 /*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 21:12:39 by chyeok            #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2022/07/21 18:32:00 by chyeok           ###   ########.fr       */
-=======
+/*   Updated: 2022/07/29 15:18:43 by chyeok           ###   ########.fr       */
 /*   Updated: 2022/07/20 15:31:18 by chyeok           ###   ########.fr       */
->>>>>>> 43953fb5ccb073a53e6db44d1af74e858ae03233
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +27,8 @@ int		ft_putstr(char *s);
 int		ft_putptr(void *ptr);
 int		ft_putnbr(int n);
 int		ft_putnbr_ptr(unsigned long int n);
-<<<<<<< HEAD
 int		ft_putnbr_u(unsigned int n, char *base, int base_n);
 int		main(void);
-=======
 int		ft_putnbr_base(unsigned int n, char *base, int base_n);
->>>>>>> 43953fb5ccb073a53e6db44d1af74e858ae03233
 
 #endif
--- printf/main.c
+++
@@ -1,8 +0,0 @@
-#include "ft_printf.h"
-#include <stdio.h>
-
-int	main(void)
-{
-	ft_printf("%s", "dong hae mul gwa beak du san");
-	return (0);
-}
