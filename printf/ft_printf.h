c1fb87cde8a0716c56d634c6fb044b900fd9dd84
Choi Hyeok <chyeok@c1r8s4.42seoul.kr>
2022-08-22 18:12:42 +0900

    update(tested 100%)

--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,6 @@
 /*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/01 21:12:39 by chyeok            #+#    #+#             */
-/*   Updated: 2022/07/29 15:21:06 by chyeok           ###   ########.fr       */
 /*   Updated: 2022/07/20 15:31:18 by chyeok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -26,7 +25,6 @@ int		ft_putstr(char *s);
 int		ft_putptr(void *ptr);
 int		ft_putnbr(int n);
 int		ft_putnbr_ptr(unsigned long int n);
-int		ft_putnbr_u(unsigned int n, char *base, int base_n);
 int		ft_putnbr_base(unsigned int n, char *base, int base_n);
 
 #endif
--- printf/ft_printf_parts.c
+++ printf/ft_printf_parts.c
@@ -6,10 +6,9 @@
 /*   By: chyeok <chyeok@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 07:39:26 by chyeok            #+#    #+#             */
-/*   Updated: 2022/07/29 15:14:46 by chyeok           ###   ########.fr       */
+/*   Updated: 2022/07/20 15:51:36 by chyeok           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "ft_printf.h"
 
@@ -20,19 +19,6 @@ int	ft_putstr(char *s)
 		write(1, "(null)", 6);
 		return (6);
 	}
-	printf("\n%d", ft_strlen(s));
-	write(1, s, ft_strlen(s));
-	return (ft_strlen(s));
-}
-
-int	ft_putptr(void *ptr)
-{
-	int	i;
-
-	i = 0;
-	i += ft_putstr("0x");
-	i += ft_putnbr_ptr((unsigned long int)ptr);
-	return (i);
 	write(1, s, ft_strlen(s));
 	return (ft_strlen(s));
 }
